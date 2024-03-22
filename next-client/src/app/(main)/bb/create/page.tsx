c5d60d4ea1329e652fc57f17b023b7026bff3bfb
Webgoonie <webgoonie@gmail.com>
2024-03-22 19:20:38 -0400

    Completed Business And Property AutoSave And Creation With Photo Upload

--- next-client/src/app/(main)/bb/create/page.tsx
+++ next-client/src/app/(main)/bb/create/page.tsx
@@ -5,7 +5,7 @@ import { useSession } from "next-auth/react"
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 
-import { businesses } from '@/Components/navbar/categories/CategoriesBusinesses'
+import { businesses } from '@/Components/navbar/navcategories/CategoriesBusinesses'
 import ModalHeading from '@/Components/modals/ModalHeading';
 
 import CategoryInput from '@/Elements/Inputs/CategoryInput';
