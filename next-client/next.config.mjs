25d1ebb7b501505542eb11dd7526f9ea1787a503
Webgoonie <webgoonie@gmail.com>
2024-03-29 12:42:03 -0400

    Trying ignore durinb builds

--- next-client/next.config.mjs
+++ next-client/next.config.mjs
@@ -1,8 +1,15 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
     generateBuildId: async () => {
     // This could be anything, using the latest git hash
       return process.env.GIT_HASH
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
     },
     reactStrictMode: true,
     swcMinify: true,
