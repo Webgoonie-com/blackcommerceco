252da349b818b72dfbde8fa783d836665b781e52
Webgoonie <webgoonie@gmail.com>
2024-04-11 01:50:30 -0400

    removed  Hit Countries on Geography Router console.log

--- api-prisma/src/geography/geography.router.ts
+++ api-prisma/src/geography/geography.router.ts
@@ -7,23 +7,31 @@ import * as GeographyController from "./geography.controller";
 export const geographyRouter = express.Router();
 
 geographyRouter.get('/allcountries', async (request: Request, response: Response) => {
-    
-    console.log('Hit Countries on Geography Router')
+
     try {
+
         const users = await GeographyController.listCountries()
         return response.status(200).json(users);
 
     } catch (error: any) {
+
         return response.status(500).json(error.message);
+
     }
+
 })
 
 geographyRouter.get('/allregions', async (request: Request, response: Response) => {
+
     try {
+
         const users = await GeographyController.listCountries()
         return response.status(200).json(users);
 
     } catch (error: any) {
+
         return response.status(500).json(error.message);
+
     }
+
 })
