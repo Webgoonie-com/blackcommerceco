a45dff64f871472236e17ba5b3bffcffbdda4506
Webgoonie <webgoonie@gmail.com>
2024-04-10 23:47:35 -0400

    Mobile Responsiveness Hero

--- next-client/src/Views/Home/Hero/Hero.tsx
+++ next-client/src/Views/Home/Hero/Hero.tsx
@@ -19,32 +19,98 @@ const Hero = () => {
     return (
         <section className="h-screen w-full bg-cover bg-center clearfix relative position-relative" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1506956191951-7a88da4435e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074')`}}>
             <Container>
-                <div className="flex flex-col h-full justify-center absolute">
-                    <div className="max-w-[49rem] px-2 me-3 md:mt-20 xl:mt-20">
+                <div className="flex flex-col justify-center absolute">
+                    <div 
+                        className="
+                        max-w-[45rem]
+                        px-2
+                        me-3
+                        mt-20
+                        sm:mt-20
+                        md:mt-20
+                        xl:mt-20
+                        ">
                         <div className="bg-zinc-700 border-2 rounded-lg border-slate-200 opacity-70 p-4">
-                            <h4 className="text-[2.4rem] leading-tight text-white font-bold">
-                                Building The African Diaspora
+                            {/* xl:text-[3.4rem]
+                            md:text-[2.3rem]
+                            sm:text-[2.3rem]
+                            xs:text-[2.4rem] */}
+                            <h4
+                                className={`
+                                        
+                                text-2xl
+                                sm:text-3xl
+                                md:text-4xl
+                                lg:text-5xl
+                                xl:text-6xl
+                                2xl:text-7xl
+                                
+                                        leading-tight
+                                        text-white
+                                        font-bold
+                                `}>
+                                Connecting The African Diaspora
                             </h4>
-                            <p className="text-base mt-2 mb-2 font-bold text-purple-500">
-                                Welcome to the Black Business 
-                                Directory And Available Properties all 
+                            <p 
+                                className="
+                                    text-base
+                                    sm:text-base
+                                    md:text-xl
+                                    lg:text-xl
+                                    xl:text-2xl
+                                    2xl:text-2xl
+                                    mt-2 
+                                    mb-2 
+                                    font-bold
+                                    text-purple-500
+                                capitalize
+                                ">
+                                Welcome to Black Business Commerce a
+                                directory of business and properties all 
                                 within a well sound ecosystem of the
-                                African Diaspora and people 
-                                throughout its nations. This is a 
+                                african diaspora and people 
+                                throughout it{"'s"} nations. This is a 
                                 community of businesses and people 
                                 who are passionate about building a better future for their people.
                             </p>
-                            <p className="text-base mt-2 mb-2 font-bold text-purple-500">
+                            <p className="
+                                text-base
+                                sm:text-base
+                                md:text-xl
+                                lg:text-xl
+                                xl:text-2xl
+                                2xl:text-2xl
+                                mt-2 mb-2 font-bold text-purple-500">
                                 We hold our destiny in our own hands. No one else will come to save us but us.
                             </p>
     
-                            <p className="text-base mt-2 mb-4 font-bold text-purple-500">Today we rise to the occasion to make a positive difference in our world and future.</p>
+                            <p className="
+                                mt-2 mb-4 font-bold text-purple-500
+                                text-base
+                                    sm:text-base
+                                    md:text-xl
+                                    lg:text-xl
+                                    xl:text-2xl
+                                    2xl:text-2xl
+                            ">
+                                Today we rise to the occasion to make a positive difference in our world and future.
+                            </p>
     
                             {!session?.user ? (
-                                <h2 className="text-[1.4rem] leading-tight text-white font-bold">Join Us Today...</h2>
+                                <h2 className="
+                                    text-[1.4rem]
+                                    sm:text-2xl
+                                    leading-tight
+                                    text-white
+                                    font-bold
+                                    ">
+                                        Join Us Today...
+                                    </h2>
                             ) : null}
 
-                            <div className="flex items-center space-x-3 mt-5">
+                            <div className="
+                                flex items-center space-x-3 mt-5
+                                ">
                                 {!session?.user ? (
                                     <>
                                         <button onClick={loginModal.onOpen} className="bg-purple-500 text-white outline-none border-2 border-purple-600  rounded-md text-sm font-semibold px-6 py-3">
@@ -55,7 +121,18 @@ const Hero = () => {
                                         </button>
                                     </>
                                 ) : (
-                                    <button className="bg-purple-500  outline-none border-2 border-purple-600 text-white-50 rounded-md text-sm font-semibold px-6 py-3">
+                                    <button className="
+                                        bg-purple-500
+                                         outline-none border-2
+                                         border-purple-600
+                                         text-white-50
+                                         rounded-md
+                                         font-semibold px-6 py-3
+                                         text-xl
+                                         sm:text-xl
+                                         md:text-lg
+                                         xl:text-xl
+                                    ">
                                         Welcome {session.user?.name}!
                                     </button>
                                 )}
