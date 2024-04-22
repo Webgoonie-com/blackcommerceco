43f4e0f671b7fe8d6e296a2cf726cc88ce3ce00d
Webgoonie <webgoonie@gmail.com>
2024-04-22 13:54:11 -0400

    Admin Login Form design layout

--- next-client/src/app/(admin)/admin/page.tsx
+++ next-client/src/app/(admin)/admin/page.tsx
@@ -1,14 +1,38 @@
+"use client"
+
 import React from 'react'
+import Image from 'next/image'
+import AdminLogo from '../../../../public/images/logo.png'
+
+import './Adminauth.css'
 
 const AdminPage = () => {
-  return (
-    <div>
-        <h2>Login As An Admin</h2>
-        <p>
-            This is Admin Login Page
-        </p>
-    </div>
-  )
+
+    function SignUp(){
+      return(
+        <div className="a-right">
+          
+        </div>
+      )
+    }
+
+    return (
+      <div className="Adminauth">
+
+          <div className="admin-left">
+            
+            <Image src={AdminLogo} width={200} height={100} alt="" />
+
+            <div className="Webname">
+              <h1 className="font-bold mb-5">Admin Login</h1>
+              <h6 className='font-semibold text-base'>Explore The shit you need to do.</h6>
+            </div>
+
+          </div>
+
+          <h1 className='font-bold text-3xl'>Form</h1>
+      </div>
+    )
 }
 
 export default AdminPage
